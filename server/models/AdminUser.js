5f0a12fcdc06b8a213d6a4b8605b61364dd7766a
adminthw <154589317@qq.com>
2021-05-15 16:07:03 +0800

    管理员账号模型完成

---
+++ server/models/AdminUser.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+  },
+
+  password: {
+    type: String,
+    select:false,
+    set(val) {
+      return require('bcrypt').hasSync(val,11)
+    }
+  },
+});
+module.exports = mongoose.model("AdminUser", schema, "adminuser");
